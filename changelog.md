3dd7c265f7ae17b8dfe4814c52080e16348ce755
Brennan Ward <Bward7864@gmail.com>
2021-11-19 02:17:13 -0500

    fix extension point

--- changelog.md
+++ changelog.md
@@ -1,3 +1,6 @@
+## 1.0.1
+* Fixed the "X" being present on server connect if installed only on the server.
+
 ## 1.0.0
 * Initial Release
 * Added an overwrite mixin that changes how the game selects y-levels for entity spawning.
--- src/main/java/shadows/spawnnorms/SpawnNormalization.java
+++ src/main/java/shadows/spawnnorms/SpawnNormalization.java
@@ -1,11 +1,14 @@
 package shadows.spawnnorms;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(SpawnNormalization.MODID)
 public class SpawnNormalization {
@@ -15,6 +18,7 @@ public class SpawnNormalization {
 
 	public SpawnNormalization() {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, SpawnsConfig.SPEC);
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 	}
 
 }
